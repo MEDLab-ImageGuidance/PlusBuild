86e6668de68a35a3f8724bf943989fb6470fd506
James Ferguson <james.m.ferguson@vanderbilt.edu>
2019-05-15 16:54:10 -0500

    Changed the cmake files to find both includedirs

--- Modules/FindIntuitiveDaVinci.cmake
+++ Modules/FindIntuitiveDaVinci.cmake
@@ -1,6 +1,17 @@
-# Find the ISI API 1.0 libary
-# This module defines
+###############################################################################
+# Find the IntuitiveDaVinci Library
 #
+#     find_package(IntuitiveDaVinci)
+#
+# Variables defined by this module:
+#
+#  IntuitiveDaVinci_FOUND               True if da Vinci API was found
+#  IntuitiveDaVinci_INCLUDE_DIRS        The location(s) of da Vinci API headers.  
+#                                         This will be two different locations:
+#		    					            one for isi_api_types.h (part of ISI API 1.0)
+#									        one for the dv_api headers (the DLL wrapper for ISI API 1.0)
+#  IntuitiveDavinci_LIBRARY_DIR         Libraries needed to use IntuitiveDaVinci
+#  IntuitiveDaVinci_BINARY_DIR          Binaries needed to use IntuitiveDaVinci
 
 SET(IntuitiveDaVinci_SDK_PATH_HINTS 
   ../IntuitiveDaVinci
@@ -9,26 +20,43 @@ SET(IntuitiveDaVinci_SDK_PATH_HINTS
   ${CMAKE_CURRENT_BINARY_DIR}/PLTools/IntuitiveDaVinci
 )
 
-FIND_PATH(IntuitiveDaVinci_INCLUDE_DIR
+FIND_PATH(ISI_INCLUDE_DIR
   NAMES "isi_api.h" "isi_api_math.h" "isi_api_types.h"
   PATH_SUFFIXES inc
   PATHS ${IntuitiveDaVinci_SDK_PATH_HINTS}
   DOC "Include directory, i.e parent directory of the ISI API"
 )
 
-FIND_LIBRARY(IntuitiveDaVinci_LIBRARY
-  NAMES isi_api.lib
-  PATHS ${IntuitiveDaVinci_SDK_PATH_HINTS}
-  DOC "Library file for ISI API"
+SET(DV_DIR_DEFAULT DV_DIR-NOTFOUND)
+SET(DV_DIR ${DV_DIR_DEFAULT} CACHE PATH "The location of the dll build of the isi_api.")
+
+IF("${DV_DIR}" STREQUAL "${DV_DIR_DEFAULT}")
+  MESSAGE(FATAL_ERROR "Please supply a location for the dv_api dll build of isi_api.")
+ENDIF()
+
+SET(DV_INCLUDE_DIR ${DV_DIR}/Include)
+
+SET(IntuitiveDaVinci_INCLUDE_DIRS
+  "${DV_INCLUDE_DIR}"
+  "${ISI_INCLUDE_DIR}"
 )
+
+SET(IntuitiveDaVinci_LIBRARY_DIR ${DV_DIR}/bin/Release)
+SET(IntuitiveDaVinci_BINARY_DIR ${DV_DIR}/bin/Release)
 
 # handle the QUIETLY and REQUIRED arguments and set ISIAPI_FOUND to TRUE if 
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(IntuitiveDaVinci DEFAULT_MSG  IntuitiveDaVinci_LIBRARY  IntuitiveDaVinci_INCLUDE_DIR  )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(IntuitiveDaVinci DEFAULT_MSG  
+  IntuitiveDaVinci_LIBRARY_DIR
+  IntuitiveDaVinci_BINARY_DIR
+  IntuitiveDaVinci_INCLUDE_DIRS
+)
 
 SET(IntuitiveDaVinci_FOUND ${INTUITIVEDAVINCI_FOUND}) 
+MESSAGE(WARNING "IntuitiveDaVinci_INCLUDE_DIRS=${IntuitiveDaVinci_INCLUDE_DIRS}")
 IF(IntuitiveDaVinci_FOUND)
-  SET( IntuitiveDaVinci_LIBRARY ${IntuitiveDaVinci_LIBRARY} )
-  SET( IntuitiveDaVinci_INCLUDE_DIR ${IntuitiveDaVinci_INCLUDE_DIR} )
+  SET( IntuitiveDaVinci_LIBRARY_DIR ${IntuitiveDaVinci_LIBRARY_DIR} )
+  SET( IntuitiveDaVinci_BINARY_DIR ${IntuitiveDaVinci_BINARY_DIR} )
+  SET( IntuitiveDaVinci_INCLUDE_DIRS ${IntuitiveDaVinci_INCLUDE_DIRS} )
 ENDIF()
--- SuperBuild/External_PlusLib.cmake
+++ SuperBuild/External_PlusLib.cmake
@@ -254,12 +254,19 @@ IF(PLUS_USE_OvrvisionPro)
     )
 ENDIF()
 
+MESSAGE(WARNING "IntuitiveDaVinci_INCLUDE_DIRS=${IntuitiveDaVinci_INCLUDE_DIRS}")
+
 IF(PLUS_USE_IntuitiveDaVinci)
+  STRING(REPLACE ";" "|" _TMP_INCLUDE_STR "${IntuitiveDaVinci_INCLUDE_DIRS}")
   LIST(APPEND PLUSBUILD_ADDITIONAL_SDK_ARGS
-    -DIntuitiveDaVinci_INCLUDE_DIR:PATH=${IntuitiveDaVinci_INCLUDE_DIR}
-    -DIntuitiveDaVinci_LIBRARY:PATH=${IntuitiveDaVinci_LIBRARY}
+    -DIntuitiveDaVinci_INCLUDE_DIRS:STRING=${_TMP_INCLUDE_STR}
+	-DIntuitiveDaVinci_LIBRARY_DIR:PATH=${IntuitiveDaVinci_LIBRARY_DIR}
+	-DIntuitiveDaVinci_BINARY_DIR:PATH=${IntuitiveDaVinci_BINARY_DIR}
     )
 ENDIF()
+
+MESSAGE(WARNING "PLUSBUILD_ADDITIONAL_SDK_ARGS=${PLUSBUILD_ADDITIONAL_SDK_ARGS}")
+
 
 IF(PLUS_USE_MMF_VIDEO OR PLUS_USE_TELEMED_VIDEO)
   # Get latest full Windows SDK (that contains actual header and lib files)
